236a402e498fc2bb65fcd7fcd33080171c1ff7c6
John Martin <john.itvn@gmail.com>
2024-10-21 09:43:47 +0000

    chore: add banner to entrypoint and make init script can run in background

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 set -e
 
+echo "██████╗ ███████╗██╗   ██╗██████╗  ██████╗ ██╗  ██╗"
+echo "██╔══██╗██╔════╝██║   ██║██╔══██╗██╔═══██╗╚██╗██╔╝"
+echo "██║  ██║█████╗  ██║   ██║██████╔╝██║   ██║ ╚███╔╝ "
+echo "██║  ██║██╔══╝  ╚██╗ ██╔╝██╔══██╗██║   ██║ ██╔██╗ "
+echo "██████╔╝███████╗ ╚████╔╝ ██████╔╝╚██████╔╝██╔╝ ██╗"
+echo "╚═════╝ ╚══════╝  ╚═══╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝"
+echo "                                                  "
+echo "         Welcome to the DevBox container          "
+echo "                                                  "
+
+
 # Run all shell scripts in /etc/entrypoint.d/
 if [ -d "/etc/entrypoint/init.d" ]; then
   for script in /etc/entrypoint/init.d/*.sh; do
     if [ -f "$script" ]; then
-      echo "Running $script"
-      . "$script"
+      echo "Run init script: $script"
+      . "$script" &
     fi
   done
+  wait
+  echo "All init scripts run"
 fi
 
 # Execute the command passed to the entrypoint
